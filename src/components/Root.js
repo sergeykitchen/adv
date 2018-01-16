73ca4be68e25b207518eb22589bf0e49dfb0ae26
sergeykitchen <kitchenkov84@gmail.com>
2018-01-16 17:05:56 +0200

    first commit

---
+++ src/components/Root.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class Root extends Component {
+    render() {
+        return (
+            <div>
+                lala
+            </div>
+        )
+    }
+}
+
+
+
+
